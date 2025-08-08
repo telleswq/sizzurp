8d48bd4b1b91104a73c23b94e0ed02e37c59e8c4
telleswq <telleswq@gmail.com>
2025-08-08 03:02:36 -0300

    feat: add category page

--- src/components/common/category-selector.tsx
+++ src/components/common/category-selector.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 import { categoryTable } from "@/db/schema";
 
 import { Button } from "../ui/button";
@@ -6,12 +8,21 @@ interface CategorySelectorProps {
     categories: (typeof categoryTable.$inferSelect)[];
 }
 const CategorySelector = ({categories}: CategorySelectorProps) => {
-    return ( 
-    <div className="rounded-3xl p-6 bg-[#F4EFFF]">
+    return (
+      <div className="rounded-3xl bg-[#F4EFFF] p-6">
         <div className="grid grid-cols-2 gap-3">
-            {categories.map(category => <Button key={category.id} variant="ghost" className="rounded-full bg-white font-semibold text-xs">{category.name}</Button>)}
+          {categories.map((category) => (
+            <Button
+              key={category.id}
+              variant="ghost"
+              className="rounded-full bg-white text-xs font-semibold"
+            >
+              <Link href={`/category/${category.slug}`}>{category.name}</Link>
+            </Button>
+          ))}
         </div>
-    </div> );
+      </div>
+    );
 }
  
 export default CategorySelector;
