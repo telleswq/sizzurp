47beb757339e4ddaa2c42434516eeea8e14cdd72
telleswq <telleswq@gmail.com>
2025-08-08 02:09:05 -0300

    feat: add sign in with google

--- auth-schema.ts
+++ auth-schema.ts
@@ -1,6 +1,6 @@
-import { boolean, pgTable, text, timestamp } from "drizzle-orm/pg-core";
+import { pgTable, text, timestamp, boolean, integer } from "drizzle-orm/pg-core";
 
-export const user = pgTable("user", {
+export const userTable = pgTable("user_table", {
 					id: text('id').primaryKey(),
 					name: text('name').notNull(),
  email: text('email').notNull().unique(),
@@ -10,7 +10,7 @@ export const user = pgTable("user", {
  updatedAt: timestamp('updated_at').$defaultFn(() => /* @__PURE__ */ new Date()).notNull()
 				});
 
-export const session = pgTable("session", {
+export const sessionTable = pgTable("session_table", {
 					id: text('id').primaryKey(),
 					expiresAt: timestamp('expires_at').notNull(),
  token: text('token').notNull().unique(),
@@ -18,14 +18,14 @@ export const session = pgTable("session", {
  updatedAt: timestamp('updated_at').notNull(),
  ipAddress: text('ip_address'),
  userAgent: text('user_agent'),
- userId: text('user_id').notNull().references(()=> user.id, { onDelete: 'cascade' })
+ userId: text('user_id').notNull().references(()=> userTable.id, { onDelete: 'cascade' })
 				});
 
-export const account = pgTable("account", {
+export const accountTable = pgTable("account_table", {
 					id: text('id').primaryKey(),
 					accountId: text('account_id').notNull(),
  providerId: text('provider_id').notNull(),
- userId: text('user_id').notNull().references(()=> user.id, { onDelete: 'cascade' }),
+ userId: text('user_id').notNull().references(()=> userTable.id, { onDelete: 'cascade' }),
  accessToken: text('access_token'),
  refreshToken: text('refresh_token'),
  idToken: text('id_token'),
@@ -35,4 +35,13 @@ export const account = pgTable("account", {
  password: text('password'),
  createdAt: timestamp('created_at').notNull(),
  updatedAt: timestamp('updated_at').notNull()
+				});
+
+export const verification = pgTable("verification", {
+					id: text('id').primaryKey(),
+					identifier: text('identifier').notNull(),
+ value: text('value').notNull(),
+ expiresAt: timestamp('expires_at').notNull(),
+ createdAt: timestamp('created_at').$defaultFn(() => /* @__PURE__ */ new Date()),
+ updatedAt: timestamp('updated_at').$defaultFn(() => /* @__PURE__ */ new Date())
 				});
